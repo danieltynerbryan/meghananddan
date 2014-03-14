bbd3bd187bf77aa8e3b551afe9136d35ccaf5836
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2014-03-13 23:59:12 -0400

    home rebuild

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   
   def home
+  	@bigDate = '2014-6-28'.to_date
+  	@today = Date.today
+  	@days_to_go = (@bigDate - @today).to_i
   end
   
   def gallery
